5dd64d7d65c5e667a21122dfb183c49a6083bfdc
Max <56440333+s1maxx@users.noreply.github.com>
2020-12-29 05:11:14 +0200

    Add new functions for 'favourite'

--- javascript/otherFunctions.js
+++ javascript/otherFunctions.js
@@ -1,41 +1,48 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	var elem = $(".product").children("div");
-		$('input:radio[id="t1"]').change(function(){
-    if($(this).is(':checked')){
-		elem.removeClass().addClass('col-xs-6 col-sm-6 col-lg-6 col-md-6 product-left p-left');
-	}	
-});
-$('input:radio[id="t2"]').change(function(){
-    if($(this).is(':checked')){
-		elem.removeClass().addClass('col-xs-12 col-sm-12 col-lg-12 col-md-12 product-left p-left');
-    }
-});
-$.getScript("/javascript/resize.js", function(){
-	new ResizeSensor(jQuery(elem), function(){ 
-		elem.css({'transition-duration':'0.4s'});
-});
-$(window).resize(function(){
-	let element = $("#change1");
-	let width = document.documentElement.clientWidth;
-	if(width < 768) {		
-		element.css('display','none');
-   }else
-   {
-	element.css('display','block');
-   }	
-  });
-$(window).load(function () {
-	let element = $("#change1");
-let width = document.documentElement.clientWidth;
-if(width < 768) {		
-	element.css('display','none');
-}else
-{
-element.css('display','block');
-}	});
-});
+	$('input:radio[id="t1"]').change(function () {
+		if ($(this).is(':checked')) {
+			elem.removeClass().addClass('col-xs-6 col-sm-6 col-lg-6 col-md-6 product-left p-left');
+		}
+	});
+	$('input:radio[id="t2"]').change(function () {
+		if ($(this).is(':checked')) {
+			elem.removeClass().addClass('col-xs-12 col-sm-12 col-lg-12 col-md-12 product-left p-left');
+		}
+	});
+	$('.favourite').click(function () {
+		var elem = $(this);
+		if (elem.attr('class') == "favourite nope") { elem.removeClass().addClass('favourite is'); }
+		else { elem.removeClass().addClass('favourite nope'); }
+
+		elem.focus();
+		elem.blur();
+	});
+	$.getScript("/javascript/resize.js", function () {
+		new ResizeSensor(jQuery(elem), function () {
+			elem.css({ 'transition-duration': '0.4s' });
+		});
+		$(window).resize(function () {
+			let element = $("#change1");
+			let width = document.documentElement.clientWidth;
+			if (width < 768) {
+				element.css('display', 'none');
+			} else {
+				element.css('display', 'block');
+			}
+		});
+		$(window).load(function () {
+			let element = $("#change1");
+			let width = document.documentElement.clientWidth;
+			if (width < 768) {
+				element.css('display', 'none');
+			} else {
+				element.css('display', 'block');
+			}
+		});
+	});
 });
 
-setTimeout(function(){
+setTimeout(function () {
 	$('body').addClass('body_visible');
 }, 100);
