0538b790ab5b7ac346422a79255740b2ffa813ae
Max <56440333+s1maxx@users.noreply.github.com>
2020-12-30 02:43:01 +0200

    Move to php

---
+++ blog.php
@@ -0,0 +1,42 @@
+<?php require_once "config.php" ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php require_once "head.php" ?>
+
+    <title><?php echo $blog['blog'] ?></title>
+</head>
+
+<body class="body_hide">
+
+    <?php require_once "header.php" ?>
+
+    <!--Navigator start-->
+    <div class="breadcrumb">
+        <div class="container">
+            <div class="breadcrumbs-main <?php echo $_SESSION['lang'] ?>">
+                <ol class="breadcrumb">
+                    <li><a href="index.php"><?php echo $lang['home'] ?></a></li>
+                    <li class="active"><?php echo $blog['blog'] ?></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    <!--Navigator end-->
+
+    <!--headtext-start-->
+    <div class="text-center" style="height:400px">
+        <br><br><br><br><br><br>
+        <div class="container">
+            <div>
+                <h2><b><?php echo $blog['temp'] ?></b></h2>
+            </div>
+        </div>
+    </div>
+    <!--headtext-end-->
+
+    <?php require_once "footer.php" ?>
+</body>
+
+</html>
