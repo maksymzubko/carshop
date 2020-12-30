e1a10b162ee0ad56050bb5cd1f59657873e8321e
Max <56440333+s1maxx@users.noreply.github.com>
2020-12-30 02:25:45 +0200

    Translate 'cars' page

--- languages/en.php
+++ languages/en.php
@@ -6,14 +6,6 @@ $lang = array(
 "cars"=>"Cars",
 "blog"=>"Blog",
 "contact"=>"Contact",
-"about1"=>"Warranty",
-"about1text"=>"We guarantee work and support for 10 years!",
-"about2"=>"Reliability",
-"about2text"=>"Our partners are reliable.",
-"about3"=>"Quality",
-"about3text"=>"Our showroom sells the highest quality cars.",
-"buttonMainText"=>"Go To Catalog",
-"buttonHideText"=>"Click here",
 "footerH1"=>"Follow Us",
 "footerH2"=>"Information",
 "footerH3"=>"Account",
@@ -24,6 +16,27 @@ $lang = array(
 "faccp2"=>"My Favourites",
 "fstorep1"=>"Kharkiv",
 "fstorep2"=>"Klochkivska street, 188.",
-"footer"=>"All Rights Reserved"
+"footer"=>"All Rights Reserved",
+"buttonHideText"=>"Click here"
+);
+$index = array(
+    "about1"=>"Warranty",
+    "about1text"=>"We guarantee work and support for 10 years!",
+    "about2"=>"Reliability",
+    "about2text"=>"Our partners are reliable.",
+    "about3"=>"Quality",
+    "about3text"=>"Our showroom sells the highest quality cars.",
+    "buttonMainText"=>"Go To Catalog",
+);
+$catalog = array(
+    "catalog"=>"Catalog auto",
+    "c1"=>"Category",
+    "c2"=>"All Categories",
+    "c3"=>"Sportcars",
+    "c4"=>"For Family",
+    "f1"=>"Brand",
+    "f2"=>"All Brands",
+    "col"=>"Color",
+    "btn"=>"View auto"
 );
 ?>
